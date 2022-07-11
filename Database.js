ecdf12acc863a5350f8986f82275a10e3e70cff7
Jeandson Barros <geoo677@gmail.com>
2022-07-10 23:59:43 -0300

    On branch master

--- Database.js
+++ Database.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const Sequelize = require('sequelize');
 
 //https://www.luiztools.com.br/post/tutorial-de-crud-com-node-js-sequelize-e-mysql/
-const sequelize = new Sequelize('suas_atividades', 'root', '', {dialect: 'mysql', host: 'localhost'});
+//const sequelize = new Sequelize('suas_atividades', 'root', '', {dialect: 'mysql', host: 'localhost'});
 
 //https://www.luiztools.com.br/post/tutorial-de-crud-com-node-js-sequelize-e-postgresql/
-//const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {dialect: 'postgres', host: process.env.HOST});
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {dialect: 'postgres', host: process.env.HOST});
 
 module.exports = sequelize;
