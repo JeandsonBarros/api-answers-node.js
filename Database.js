9fb38b508a64e3258824a06acdc163b8d72cb5f9
Jeandson Barros <geoo677@gmail.com>
2022-07-15 19:50:07 -0300

    Adição de novas funções

--- Database.js
+++ Database.js
@@ -4,10 +4,10 @@ const Sequelize = require('sequelize');
 
 /* ===== If you want to use mysql =====*/
 //Tips: https://www.luiztools.com.br/post/tutorial-de-crud-com-node-js-sequelize-e-mysql/
-//const sequelize = new Sequelize('suas_questoes', 'root', '', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('suas_questoes', 'root', '', {dialect: 'mysql', host: 'localhost'});
 
 /* ===== If you want to use postgresql =====*/
 //Tips: https://www.luiztools.com.br/post/tutorial-de-crud-com-node-js-sequelize-e-postgresql/
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {dialect: 'postgres', host: process.env.HOST});
+//const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {dialect: 'postgres', host: process.env.HOST});
 
 module.exports = sequelize;
